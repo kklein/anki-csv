f43e9f7cd23a85260bd0dc5674d9e2874a321360
kklein <kleinkevin.lu@gmail.com>
2023-06-16 23:33:00 +0200

    Update tests.

--- tests/test_ankicsv.py
+++ tests/test_ankicsv.py
@@ -1,29 +1,22 @@
+from pathlib import Path
 import pytest
 
-from ankicsv import validate
+from ankicsv import validate_file
 
 
-@pytest.mark.parametrize("input, expected_success", [
-    ("asdf, asdf", True),
-    ("""asdf, "asdf" """, True),
-    ("asdf, asdf, asdf", False),
-    (""" asdf, "asdf, asdf" """, True),
-    ("""asdf, asdf
-    asdf, asdf""", True),
-    ("""asdf, asdf
-    asdf,""", False),
-    ("""asdf, asdf
-    asdf, "asdf
-    asdf
-    asdf
-    "
-    asdf, asdf""", True),
+@pytest.mark.parametrize("file, expected_success", [
+    ("missing_value.csv", False),
+    ("duplicate_key.csv", False),
+    ("superfluous_comma.csv", False),
+    ("multiline.csv", True),
+    ("escaped_comma.csv", True),
 ])
-def test_validate_single_line(input, expected_success):
+def test_validate(file, expected_success):
+    path = Path(__file__).parent / file
+    print(path)
     if expected_success:
-        validate(input)
+        validate_file(path)
     else:
         with pytest.raises(ValueError):
-            validate(input)
-
+            validate_file(path)
 
