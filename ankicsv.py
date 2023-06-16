866af68ae079ecd8545d0e3a3e332f6bf880e971
kklein <kleinkevin.lu@gmail.com>
2023-06-16 23:36:19 +0200

    Formatting.

--- ankicsv.py
+++ ankicsv.py
@@ -1,7 +1,8 @@
-from pathlib import Path
-import re
-import click
 import csv
+import re
+from pathlib import Path
+
+import click
 
 
 def validate_string(body: str) -> None:
@@ -18,10 +19,7 @@ def validate_lines(lines: list):
         if not isinstance(line, list):
             raise ValueError()
         if len(line) != 2:
-            raise ValueError(
-                "A line had more than 2 values: \n"
-                f"{str(line)}"
-            )
+            raise ValueError(f"A line had more than 2 values: \n {str(line)}")
         new_key = line[0].strip()
         if new_key in keys:
             raise ValueError(f"Key {new_key} occurs several times.")
@@ -32,7 +30,11 @@ def validate_file(file: Path) -> None:
     with open(file, "r") as fh:
         print(f"Validating {file}.")
         reader = csv.reader(
-            fh, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True
+            fh,
+            quotechar='"',
+            delimiter=",",
+            quoting=csv.QUOTE_ALL,
+            skipinitialspace=True,
         )
         validate_lines(list(reader))
 
--- tests/test_ankicsv.py
+++ tests/test_ankicsv.py
@@ -1,16 +1,20 @@
 from pathlib import Path
+
 import pytest
 
 from ankicsv import validate_file
 
 
-@pytest.mark.parametrize("file, expected_success", [
-    ("missing_value.csv", False),
-    ("duplicate_key.csv", False),
-    ("superfluous_comma.csv", False),
-    ("multiline.csv", True),
-    ("escaped_comma.csv", True),
-])
+@pytest.mark.parametrize(
+    "file, expected_success",
+    [
+        ("missing_value.csv", False),
+        ("duplicate_key.csv", False),
+        ("superfluous_comma.csv", False),
+        ("multiline.csv", True),
+        ("escaped_comma.csv", True),
+    ],
+)
 def test_validate(file, expected_success):
     path = Path(__file__).parent / file
     print(path)
@@ -19,4 +23,3 @@ def test_validate(file, expected_success):
     else:
         with pytest.raises(ValueError):
             validate_file(path)
-
